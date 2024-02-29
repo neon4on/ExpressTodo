8d9085917a9f2bec5c2b31880874ad7c0d089960
Valeriy Kamenskikh <86181396+neon4on@users.noreply.github.com>
2024-02-29 07:16:58 +0000

    init

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const todoRoutes = require('./routes/todos');
+const exphbs = require('express-handlebars');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs',
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+app.use(todoRoutes);
+
+async function start(params) {
+  try {
+    await mongoose.connect(
+      'mongodb+srv://rushexe:kkkk1812@cluster0.rnfzwlz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    );
+    app.listen(PORT, () => {
+      console.log('Server has been started...');
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+start();
